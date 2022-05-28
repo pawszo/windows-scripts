62bc311dd79dc4ff8bc80e1db327c1a8d9ab86c0
PSZ <paulszoltysek@gmail.com>
2022-05-28 22:14:46 +0200

    add dynamic toggle

--- toggleTouchPad.ps1
+++ toggleTouchPad.ps1
@@ -1,1 +1,8 @@
-Get-PnpDevice | Where-Object {$_.Name -like '*Synaptics TPFHID*'} | Disable-PnpDevice -Confirm:$false
+$touchpad = Get-PnpDevice | Where-Object {$_.FriendlyName -like '*TPFHID*'} 
+
+if($touchpad.Status.equals("Error")) {
+Get-PnpDevice | Where-Object {$_.FriendlyName -like '*TPFHID*'} | Enable-PnpDevice -Confirm:$false
+}
+else {
+Get-PnpDevice | Where-Object {$_.FriendlyName -like '*TPFHID*'} | Disable-PnpDevice -Confirm:$false
+}
